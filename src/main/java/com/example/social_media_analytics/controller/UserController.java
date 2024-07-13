9518cbf2ebf081e838c6d5f7d2104cdfe1de94c6
sanchit-cell <70786565+sanchit-cell@users.noreply.github.com>
2024-07-14 03:19:18 +0530

    Update UserController.java

--- src/main/java/com/example/social_media_analytics/controller/UserController.java
+++ src/main/java/com/example/social_media_analytics/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://social-media-analytics-dashboard.onrender.com")
 public class UserController {
     @Autowired
     private UserService userService;
