885b987ef1eb09984eba7d374606ef82e6f292fe
sanchit-cell <70786565+sanchit-cell@users.noreply.github.com>
2024-07-17 11:26:34 +0530

    Update UserController.java

--- src/main/java/com/example/social_media_analytics/controller/UserController.java
+++ src/main/java/com/example/social_media_analytics/controller/UserController.java
@@ -14,7 +14,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserService userService;
